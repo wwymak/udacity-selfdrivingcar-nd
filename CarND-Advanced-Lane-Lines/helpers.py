dff1b71dc5733d08c9ef0e5087fff090cf076b3b
wwymak <wwymak@gmail.com>
2017-12-22 06:45:46 +0000

    wip

--- CarND-Advanced-Lane-Lines/helpers.py
+++ CarND-Advanced-Lane-Lines/helpers.py
@@ -6,6 +6,43 @@ def img2Gray(filename):
     img = cv2.imread(filename)
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    """
+    This function draws `lines` with `color` and `thickness`.
+    Lines are drawn on the image inplace (mutates the image).
+    If you want to make the lines semi-transparent, think about combining
+    this function with the weighted_img() function below
+    """
+    for line in lines:
+        x1 = line[0]
+        y1 = line[1]
+        x2 = line[2]
+        y2 = line[3]
+#         for x1,y1,x2,y2 in line:
+        cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+    return img
+
+def order_points(points):
+    """
+    ensure that the pts list are ordered from top left in clockwise
+    direction for perspectiveTransform
+    """
+    rect = np.zeros((4, 2), dtype = "float32")
+    coordsSum = np.sum(points, axis=1)
+    coordsDiff = np.diff(points, axis=1)
+    # top left point has smallest sum
+    rect[0] = points[np.argmin(coordsSum)]
+    # bottom right point has biggest sum
+    rect[2] = points[np.argmax(coordsSum)]
+    # top right point has smallest (in fact, can be -ve difference)
+    rect[1] = points[np.argmin(coordsDiff)]
+    #  and the bottom left the highest
+    rect[3] = points[np.argmax(coordsDiff)]
+	# return the ordered coordinates
+
+    return rect
+
+
 def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     if orient == 'x':
