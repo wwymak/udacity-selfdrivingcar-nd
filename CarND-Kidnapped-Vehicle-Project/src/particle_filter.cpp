ccb0dbb2626a3839babfce69181cfa7c1bb5fed3
wwymak <wwymak@gmail.com>
2018-03-22 21:59:56 +0000

    still debugging...

--- CarND-Kidnapped-Vehicle-Project/src/particle_filter.cpp
+++ CarND-Kidnapped-Vehicle-Project/src/particle_filter.cpp
@@ -22,33 +22,36 @@ using namespace std;
 
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
-    default_random_engine gen;
-	// Set the number of particles. Initialize all particles to first position (based on estimates of
-	//   x, y, theta and their uncertainties from GPS) and all weights to 1.
-	// Add random Gaussian noise to each particle.
-    num_particles = 10;
 
-    double std_x = std[0];
-    double std_y = std[0];
-    double std_theta = std[0];
+    if(! is_initialized){
+        default_random_engine gen;
+        // Set the number of particles. Initialize all particles to first position (based on estimates of
+        //   x, y, theta and their uncertainties from GPS) and all weights to 1.
+        // Add random Gaussian noise to each particle.
+        num_particles = 10;
 
-    normal_distribution<double> dist_x(x, std_x);
-    normal_distribution<double> dist_y(y, std_y);
-    normal_distribution<double> dist_theta(theta, std_theta);
+        double std_x = std[0];
+        double std_y = std[1];
+        double std_theta = std[2];
 
-    for(int i = 0; i< num_particles; i++) {
-        Particle p;
-        p.id = i;
-        p.x = dist_x(gen);
-        p.y = dist_y(gen);
-        p.theta = dist_theta(gen);
-        p.weight = 1.0;
+        normal_distribution<double> dist_x(x, std_x);
+        normal_distribution<double> dist_y(y, std_y);
+        normal_distribution<double> dist_theta(theta, std_theta);
 
-        particles.push_back(p);
-        weights.push_back(1.0);
+        for(int i = 0; i< num_particles; i++) {
+            Particle p;
+            p.id = i;
+            p.x = dist_x(gen);
+            p.y = dist_y(gen);
+            p.theta = dist_theta(gen);
+            p.weight = 1.0;
+
+            particles.push_back(p);
+            weights.push_back(1.0);
+        }
+
+        is_initialized = true;
     }
-
-    is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
@@ -56,11 +59,10 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
     default_random_engine gen;
 
     double std_x = std_pos[0];
-    double std_y = std_pos[0];
-    double std_theta = std_pos[0];
+    double std_y = std_pos[1];
+    double std_theta = std_pos[2];
 
-//    particles.clear();
-    for (int i = 0; i< particles.size(); i++) {
+    for (int i = 0; i< num_particles; i++) {
         double x0, x1, y0, y1, theta0, theta1;
 
         Particle p_i = particles.at(i);
@@ -69,14 +71,14 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
         y0 = p_i.y;
         theta0 = p_i.theta;
 
-        if (yaw_rate == 0) {
+        if (fabs(yaw_rate) < 0.0001) {
             x1 = x0 + velocity  * cos(p_i.theta) * delta_t;
             y1 = y0 +  velocity  * sin(p_i.theta) * delta_t;
-            theta1 = theta0;
+            theta1 = theta0 + 0.0001 * delta_t;
         } else {
             x1 = x0 +  velocity / yaw_rate * (sin(theta0 + yaw_rate * delta_t) - sin(theta0));
             y1 = y0 + velocity / yaw_rate * (cos(theta0) - cos(theta0 + yaw_rate * delta_t));
-            theta1 += yaw_rate * delta_t;
+            theta1 = theta0 +  yaw_rate * delta_t;
         }
 
         normal_distribution<double> dist_x(x1, std_x);
@@ -91,31 +93,32 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 }
 
 void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
-	// TODO: Find the predicted measurement that is closest to each observed measurement and assign the
+	//  Find the predicted measurement that is closest to each observed measurement and assign the
 	//   observed measurement to this particular landmark.
-	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to
-	//   implement this method and use it as a helper during the updateWeights phase.
-//    float distOar
     int closest_predicted_index = 0;
-    int min_dist;
+    double min_dist = 99999999; // a very high value such that should be overwritten
     for (int i = 0; i< observations.size(); i++) {
+        LandmarkObs curr_obs = observations.at(i);
         for (int j = 0; j< predicted.size(); j++) {
-            double dist_curr = dist(observations.at(i).x, observations.at(i).y, predicted.at(j).x, predicted.at(j).y);
-            if(j == 0) {
-                min_dist = dist_curr;
-            } else if (dist_curr < min_dist) {
+            LandmarkObs curr_predicted = predicted.at(j);
+            double dist_curr = dist(curr_obs.x, curr_obs.y, curr_predicted.x, curr_predicted.y);
+            if (dist_curr < min_dist) {
                 min_dist = dist_curr;
                 closest_predicted_index = j;
             }
         }
-
+        curr_obs.id = closest_predicted_index;
     }
+
+    cout << "Data association end"<< endl;
 
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 		const std::vector<LandmarkObs> &observations, const Map &map_landmarks) {
     weights.clear();
+    const double std_landmark_x = std_landmark[0];
+    const double std_landmark_y = std_landmark[1];
 	// TODO: Update the weights of each particle using a mult-variate Gaussian distribution. You can read
 	//   more about thias distribution here: https://en.wikipedia.org/wiki/Multivariate_normal_distribution
 	// NOTE: The observations are given in the VEHICLE'S coordinate system. Your particles are located
@@ -144,12 +147,13 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 
         vector<LandmarkObs> landmarksInRange;
         for (int a = 0; a < map_landmarks.landmark_list.size(); a++) {
-            double distToMark = dist(map_landmarks.landmark_list.at(i).x_f, map_landmarks.landmark_list.at(i).y_f, p_i.x, p_i.y);
+            Map::single_landmark_s curr_landmark = map_landmarks.landmark_list.at(i);
+            double distToMark = dist(curr_landmark.x_f, curr_landmark.y_f, p_i.x, p_i.y);
             if (distToMark <= sensor_range) {
                 LandmarkObs obs_in_range;
-                obs_in_range.id = map_landmarks.landmark_list.at(i).id;
-                obs_in_range.x = map_landmarks.landmark_list.at(i).x_f;
-                obs_in_range.y =  map_landmarks.landmark_list.at(i).y_f;
+                obs_in_range.id = curr_landmark.id_i;
+                obs_in_range.x = curr_landmark.x_f;
+                obs_in_range.y =  curr_landmark.y_f;
                 landmarksInRange.push_back(obs_in_range);
             }
         }
@@ -157,7 +161,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
         vector<LandmarkObs> transformed_obs;
         for (int j = 0; j < observations.size(); j++) {
             LandmarkObs obs = observations.at(j);
-            cout <<"here 2" << endl;
+
             LandmarkObs obs_trans;
             obs_trans.id = obs.id;
             double xm = p_i.x + cos(p_i.theta) * obs.x - obs.y * sin(p_i.theta);
@@ -165,50 +169,76 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
             obs_trans.x = xm;
             obs_trans.y = ym;
             transformed_obs.push_back(obs_trans);
+        }
 
+        p_i.weight = 1.0;
 
-//            <Map::single_landmark_s> minLandmark = map_landmarks.landmark_list.at(0);
-            double minDistance = dist(map_landmarks.landmark_list.at(0).x_f, map_landmarks.landmark_list.at(0).y_f, xm, ym);
-            float minX = map_landmarks.landmark_list.at(0).x_f;
-            float minY = map_landmarks.landmark_list.at(0).y_f;
-            int index = 0;
-//            double minDistance = dist(minLandmark.x_f, minLandmark.y_f, xm, ym);
+        //the transformed_obs will each have their ids = the closest landmark's id
+        dataAssociation(landmarksInRange, transformed_obs);
+        cout <<"here 4" << endl;
+        cout <<transformed_obs.size() << endl;
+        // find the associated landmark by looping over the landmarks in range
+        for (int k = 0; k< transformed_obs.size(); k++) {
+            LandmarkObs curr_obst = transformed_obs.at(k);
+            double xm = curr_obst.x;
+            double ym = curr_obst.y;
+            int curr_id = curr_obst.id;
 
-            for  (int k = 1; k< map_landmarks.landmark_list.size(); k++ ){
-//                <Map::single_landmark_s> currLandmark = map_landmarks.landmark_list.at(k);
-                double currDist = dist(map_landmarks.landmark_list.at(k).x_f, map_landmarks.landmark_list.at(k).y_f, xm, ym);
-                if (currDist < minDistance) {
-                    minDistance = currDist;
-                    index = k;
-                    minX = map_landmarks.landmark_list.at(k).x_f;
-                    minY = map_landmarks.landmark_list.at(k).y_f;
+            double mu_x;
+            double mu_y;
+
+            for (int l= 0; l< landmarksInRange.size(); l++) {
+                if (landmarksInRange.at(l).id == curr_id) {
+                    mu_x = landmarksInRange.at(l).x;
+                    mu_y = landmarksInRange.at(l).y;
                 }
             }
+            cout << "asoociated"<< endl;
+            double powx =  pow((xm - mu_x), 2) /(2 * pow(std_landmark_x, 2));
+            double powy =  pow((ym - mu_y), 2) /(2 * pow(std_landmark_y, 2));
 
-            double powx =  pow((xm - minX), 2) /(2 * pow(std_landmark[0], 2));
-            double powy =  pow((ym - minY), 2) /(2 * pow(std_landmark[1], 2));
-
-//            double powx =  pow((xm - minLandmark.x_f), 2) /(2 * pow(std_landmark[0], 2));
-//            double powy =  pow((ym - minLandmark.y_f), 2) /(2 * pow(std_landmark[1], 2));
-
-            p_i.weight = p_i.weight*  1 /(2 * M_PI * std_landmark[0] * std_landmark[1] ) * pow(M_E, -(powx + powy));
-
-
-
+            p_i.weight *=  1 /(2 * M_PI * std_landmark_x * std_landmark_y ) * exp( -(powx + powy));
         }
+//
+////            <Map::single_landmark_s> minLandmark = map_landmarks.landmark_list.at(0);
+//            double minDistance = dist(map_landmarks.landmark_list.at(0).x_f, map_landmarks.landmark_list.at(0).y_f, xm, ym);
+//            float minX = map_landmarks.landmark_list.at(0).x_f;
+//            float minY = map_landmarks.landmark_list.at(0).y_f;
+//            int index = 0;
+////            double minDistance = dist(minLandmark.x_f, minLandmark.y_f, xm, ym);
+//
+//            for  (int k = 1; k< map_landmarks.landmark_list.size(); k++ ){
+////                <Map::single_landmark_s> currLandmark = map_landmarks.landmark_list.at(k);
+//                double currDist = dist(map_landmarks.landmark_list.at(k).x_f, map_landmarks.landmark_list.at(k).y_f, xm, ym);
+//                if (currDist < minDistance) {
+//                    minDistance = currDist;
+//                    index = k;
+//                    minX = map_landmarks.landmark_list.at(k).x_f;
+//                    minY = map_landmarks.landmark_list.at(k).y_f;
+//                }
+//            }
+//
+//            double powx =  pow((xm - minX), 2) /(2 * pow(std_landmark[0], 2));
+//            double powy =  pow((ym - minY), 2) /(2 * pow(std_landmark[1], 2));
+//
+////            double powx =  pow((xm - minLandmark.x_f), 2) /(2 * pow(std_landmark[0], 2));
+////            double powy =  pow((ym - minLandmark.y_f), 2) /(2 * pow(std_landmark[1], 2));
+//
+//            p_i.weight = p_i.weight*  1 /(2 * M_PI * std_landmark[0] * std_landmark[1] ) * pow(M_E, -(powx + powy));
+//
+//
+//
+//        }
         weights.push_back(p_i.weight);
-        particles.at(i) = p_i;
-
-        cout << "new weight"<< weights.at(i)<< endl;
-        if (weights.at(i) == 1) {
-            cout << "pi weidght:"<< i<< ","<< p_i.weight << endl;
-        }
-        cout << "particles" << particles.at(i).weight<<endl;
+//        particles.at(i) = p_i;
+//
+//        cout << "new weight"<< weights.at(i)<< endl;
+//        if (weights.at(i) == 1) {
+//            cout << "pi weidght:"<< i<< ","<< p_i.weight << endl;
+//        }
+//        cout << "particles" << particles.at(i).weight<<endl;
     }
-    double max_weight = *max_element(weights.begin(), weights.end());
-//    for (int j = 0; j< weights.size(); j++) {
-//        weights.at(j) = weights.at(j) / 2 * max_weight;
-//    }
+
 
 }
 
