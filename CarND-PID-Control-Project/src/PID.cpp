a2e9eb39511c1d98bc500e68d072550574ed7dc2
wwymak <wwymak@gmail.com>
2018-03-25 22:06:47 +0100

    wip

--- CarND-PID-Control-Project/src/PID.cpp
+++ CarND-PID-Control-Project/src/PID.cpp
@@ -12,11 +12,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    d_error = 0;
+    i_error = 0;
+    p_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+    i_error += cte;
+    d_error = cte - p_error;
+    p_error = cte;
 }
 
 double PID::TotalError() {
+    return Kp * p_error + Ki * i_error + Kd * d_error;
 }
 
