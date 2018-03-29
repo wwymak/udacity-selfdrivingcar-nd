42279ae04acbbaa5b66a13a935a6b7749539e075
wwymak <wwymak@gmail.com>
2018-03-29 21:19:47 +0100

    minor

--- CarND-MPC-Project/src/MPC.cpp
+++ CarND-MPC-Project/src/MPC.cpp
@@ -6,8 +6,8 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 0;
-double dt = 0;
+size_t N = 50;
+double dt = 0.1;
 
 // This value assumes the model presented in the classroom is used.
 //
